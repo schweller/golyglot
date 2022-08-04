80aa5ebb064d59c51c14da347a6d50835ecacc7f
Inácio Schweller <iscmendonca@gmail.com>
2022-08-04 16:25:17 +0200

    feat: text command fully working

---
+++ error.go
@@ -0,0 +1,23 @@
+package rumor
+
+import "fmt"
+
+type errorWithCode struct {
+	message string
+	code    int
+}
+
+func (e *errorWithCode) Error() string {
+	return e.message
+}
+
+func (e *errorWithCode) Code() int {
+	return e.code
+}
+
+func newErrorWithCode(code int, message string, args ...any) *errorWithCode {
+	return &errorWithCode{
+		message: fmt.Sprintf(message, args...),
+		code:    code,
+	}
+}
